4cb2d2cc7f16719e983ecf85032e5303ff017a80
Qingcai <qingcai518@gmail.com>
2018-08-31 11:08:40 +0900

    add linechart

--- DigitalReceipt/src/pages/chart/ChartController.swift
+++ DigitalReceipt/src/pages/chart/ChartController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Charts
 
 class ChartController: ViewController {
-    let chartView = CombinedChartView()
+    let chartView = LineChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,22 +25,10 @@ class ChartController: ViewController {
     private func setSubViews() {
         view.backgroundColor = UIColor.white
         
-        // add charts.
-        let chartData = CombinedChartData()
-        chartData.lineData
-         = generateLineData()
-        
-        let titleLbl = UILabel()
-        titleLbl.textColor = UIColor.black
-        titleLbl.numberOfLines = 0
-        titleLbl.textColor = UIColor.orange
-        titleLbl.text = "コンテント"
-        titleLbl.font = UIFont.systemFont(ofSize: 12)
-        self.view.addSubview(titleLbl)
-        titleLbl.snp.makeConstraints { make in
+        view.addSubview(chartView)
+        chartView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(24)
-            make.left.right.equalToSuperview().inset(24)
-            make.height.equalTo(20)
+            make.left.right.equalToSuperview()
         }
     }
 }
