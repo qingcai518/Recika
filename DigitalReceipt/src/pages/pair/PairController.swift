7fe4dfa0ba579072502353312be45d3db34a8ccd
Qingcai <qingcai518@gmail.com>
2018-09-03 10:37:44 +0900

    add pair

---
+++ DigitalReceipt/src/pages/pair/PairController.swift
@@ -0,0 +1,20 @@
+//
+//  PairController.swift
+//  DigitalReceipt
+//
+//  Created by liqc on 2018/09/03.
+//  Copyright © 2018年 liqc. All rights reserved.
+//
+
+import UIKit
+
+class PairController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
