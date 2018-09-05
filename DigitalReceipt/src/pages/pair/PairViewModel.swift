490a1ec07b40db6df5a0422c2ceb39818a3aad42
Qingcai <qingcai518@gmail.com>
2018-09-05 18:20:59 +0900

    change to post

--- DigitalReceipt/src/pages/pair/PairViewModel.swift
+++ DigitalReceipt/src/pages/pair/PairViewModel.swift
@@ -63,23 +63,24 @@ class PairViewModel {
                         print("can not get data")
                         return
                     }
-                    print(priceData)
+                    
+                    print(data)
+                    price.latestPrice.value = priceData.latestPrice.value
                 }
             }
         }
     }
     
+    // do post request.
     func getTicker(from: String, to: String, completion: @escaping (PriceData?, String?) -> Void) {
-        guard var api = URLComponents(string: tickerAPI) else {
+        guard let api = URLComponents(string: tickerAPI) else {
             return completion(nil, "can not found url")
         }
-        api.queryItems = [
-            URLQueryItem(name: "from", value: from),
-            URLQueryItem(name: "to", value: to)
-        ]
         
+        let params = ["from": from, "to": to]
+        let headers = ["Content-Type": "application/json"]
         print(api)
-        Alamofire.request(api, method: .get).responseJSON { response in
+        Alamofire.request(api, method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
             if let error = response.error {
                 return completion(nil, error.localizedDescription)
             }
@@ -92,8 +93,36 @@ class PairViewModel {
             print(json)
             let latest = json["latest"].doubleValue
             let priceData = PriceData(tokenName: to, latestPrice: latest)
-            
             return completion(priceData, nil)
         }
     }
+    
+    // do get request.
+//    func getTicker(from: String, to: String, completion: @escaping (PriceData?, String?) -> Void) {
+//        guard var api = URLComponents(string: tickerAPI) else {
+//            return completion(nil, "can not found url")
+//        }
+//        api.queryItems = [
+//            URLQueryItem(name: "from", value: from),
+//            URLQueryItem(name: "to", value: to)
+//        ]
+//
+//        print(api)
+//        Alamofire.request(api, method: .get).responseJSON { response in
+//            if let error = response.error {
+//                return completion(nil, error.localizedDescription)
+//            }
+//
+//            guard let data = response.data else {
+//                return completion(nil, "fail to get data")
+//            }
+//
+//            let json = JSON(data)
+//            print(json)
+//            let latest = json["latest"].doubleValue
+//            let priceData = PriceData(tokenName: to, latestPrice: latest)
+//
+//            return completion(priceData, nil)
+//        }
+//    }
 }
