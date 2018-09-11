3d4f22317c4536cdff30c9fe9fc7880fbd0a39c0
Qingcai <qingcai518@gmail.com>
2018-09-11 12:02:59 +0900

    try to use obj-c appdelegate

---
+++ DigitalReceipt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DigitalReceipt
+//
+//  Created by liqc on 2018/09/11.
+//  Copyright © 2018年 liqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
