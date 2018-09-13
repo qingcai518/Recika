2e6eaa7f20f45d4773563b1b2679bd7a7058f049
Qingcai <qingcai518@gmail.com>
2018-09-13 17:16:57 +0900

    add ResultController

---
+++ Recika/src/pages/result/ResultController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultController.swift
+//  Recika
+//
+//  Created by liqc on 2018/09/13.
+//  Copyright © 2018年 liqc. All rights reserved.
+//
+
+import UIKit
+
+class ResultController: ViewController {
+    let tableView = UITableView()
+    let topView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSubViews()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    fileprivate func setupSubViews() {
+        view.addSubView(topView)
+        view.addSubView(tableView)
+    }
+}
