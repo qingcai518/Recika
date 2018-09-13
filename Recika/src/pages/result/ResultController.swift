3d45d0fc45384857351a913e6d1dc4b9f7e2e7c5
Qingcai <qingcai518@gmail.com>
2018-09-13 17:33:47 +0900

    add content for result controller

--- Recika/src/pages/result/ResultController.swift
+++ Recika/src/pages/result/ResultController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ResultController: ViewController {
     let tableView = UITableView()
     let topView = UIView()
+    let dateLbl = UILabel()
+    let telLbl = UILabel()
+    let totalPriceLbl = UILabel()
+    let adjustPriceLbl = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +29,34 @@ class ResultController: ViewController {
     fileprivate func setupSubViews() {
         view.addSubview(topView)
         view.addSubview(tableView)
+        
+        topView.addSubview(dateLbl)
+        topView.addSubview(telLbl)
+        topView.addSubview(totalPriceLbl)
+        topView.addSubview(adjustPriceLbl)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
+
+extension ResultController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ResultController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
