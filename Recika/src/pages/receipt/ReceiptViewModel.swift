81cdb3aff0e45e9244c4a7644abd79c59d26b302
Qingcai <qingcai518@gmail.com>
2018-09-19 14:54:37 +0900

    change get receipt logic

--- Recika/src/pages/receipt/ReceiptViewModel.swift
+++ Recika/src/pages/receipt/ReceiptViewModel.swift
@@ -14,18 +14,16 @@ import SVProgressHUD
 class ReceiptViewModel {
     var receipts = [ReceiptData]()
     
-    func getReceiptData(completion: @escaping (String?) -> Void) {
+    func getReceiptData(completion: @escaping (String?)-> Void) {
         guard let api = URLComponents(string: receiptAPI) else {
             return completion("can not get api")
         }
-        
         SVProgressHUD.show()
         Alamofire.request(api, method: .get).responseJSON { [weak self] response in
             SVProgressHUD.dismiss()
             if let error = response.error {
                 return completion(error.localizedDescription)
             }
-            
             guard let data = response.data else {
                 return completion("fail to get response data")
             }
