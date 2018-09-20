1ebe7920211d8ab9a4bbb9f2d66916523e2a97a8
Qingcai <qingcai518@gmail.com>
2018-09-20 17:42:13 +0900

    add pull to refresh

--- Recika/src/pages/receipt/ReceiptController.swift
+++ Recika/src/pages/receipt/ReceiptController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RxSwift
+import ESPullToRefresh
 
 class ReceiptController: ViewController {
     var collectionView: UICollectionView!
@@ -56,10 +57,21 @@ class ReceiptController: ViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(ReceiptCell.self, forCellWithReuseIdentifier: ReceiptCell.id)
+        collectionView.alwaysBounceVertical = true
+        collectionView.es.addPullToRefresh { [weak self] in
+            self?.getData()
+        }
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.left.right.equalToSuperview()
+        }
     }
     
     private func getData() {
         viewModel.getReceiptData { [weak self] msg in
+            self?.collectionView.es.stopPullToRefresh()
+            
             if let msg = msg {
                 self?.showToast(text: msg)
             } else {
