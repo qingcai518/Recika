0e71f0be460bce316663763e3962faf4248afe59
Qingcai <qingcai518@gmail.com>
2018-09-27 17:08:22 +0900

    change domain to java server

--- Recika/src/common/network/NetConstants.swift
+++ Recika/src/common/network/NetConstants.swift
@@ -8,17 +8,13 @@
 
 import Foundation
 
-// 本地环境
-//let cybexDomain = "http://localhost:8081"
-//let domain = "http://localhost:8080"
-
-// 47服务器环境
-//let cybexDomain = "http://47.91.242.71:8088"
-//let domain = "http://47.91.242.71:8085"
-
 // bc tecth环境
+// Python API.
 let cybexDomain = "http://52.197.86.134:8081"
-let domain = "http://52.197.86.134:8085"
+// Go API.
+//let domain = "http://52.197.86.134:8085"
+// Java API.
+let domain = "http://52.197.86.134:8080"
 
 let uploadDomain = "https://main-myreader.ssl-lolipop.jp/recika_upload"    // 暂时用
 
