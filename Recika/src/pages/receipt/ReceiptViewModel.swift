6a4cf233d2676696f8e15ee554bd093f98e60f87
Qingcai <qingcai518@gmail.com>
2018-09-28 12:24:28 +0900

    add load more and refresh

--- Recika/src/pages/receipt/ReceiptViewModel.swift
+++ Recika/src/pages/receipt/ReceiptViewModel.swift
@@ -13,23 +13,49 @@ import SVProgressHUD
 
 class ReceiptViewModel {
     var receipts = [ReceiptData]()
+    var page = 0
+    let size = 3
+    
+    func refresh(completion: @escaping (String?) -> Void) {
+        page = 0
+        receipts = [ReceiptData]()
+        getReceiptData { msg in
+            return completion(nil)
+        }
+    }
+    
+    func loadMore(completion: @escaping (String?) -> Void) {
+        if page == -1 {return}
+        getReceiptData { msg in
+            return completion(nil)
+        }
+    }
     
     func getReceiptData(completion: @escaping (String?)-> Void) {
-        guard let api = URLComponents(string: receiptAPI) else {
+        guard var api = URLComponents(string: receiptAPI) else {
             return completion("can not get api")
         }
+        
+        api.queryItems = [
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "size", value: "\(size)")
+        ]
+        
         SVProgressHUD.show()
         Alamofire.request(api, method: .get).responseJSON { [weak self] response in
             SVProgressHUD.dismiss()
             if let error = response.error {
                 return completion(error.localizedDescription)
             }
+            
             guard let data = response.data else {
-                return completion("fail to get response data")
+                return completion("fail to get response data.")
             }
             
             let json = JSON(data)
-            self?.receipts = json.arrayValue.map{ReceiptData(json: $0)}
+            self?.page = json["next_page"].intValue
+            let receiptDatas = json["data"].arrayValue.map{ReceiptData(json: $0)}
+            self?.receipts.append(receiptDatas)
             return completion(nil)
         }
     }
