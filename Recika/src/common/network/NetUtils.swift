29b8387b2117d796eb84117890afa4aed4bde659
Qingcai <qingcai518@gmail.com>
2018-10-24 11:08:52 +0900

    add get dynamic request

--- Recika/src/common/network/NetUtils.swift
+++ Recika/src/common/network/NetUtils.swift
@@ -11,6 +11,8 @@ import Alamofire
 import SwiftyJSON
 import SVProgressHUD
 
+typealias CallbackDynamic = (DynamicChainData?, String?) -> Void
+
 func getChainId(callback: @escaping Callback) {
     if let chainId = UserDefaults.standard.object(forKey: UDKey.chainID) as? String {
         return callback(chainId)
@@ -37,24 +39,22 @@ func getChainId(callback: @escaping Callback) {
     }
 }
 
-func getDynamicChainInfo(callback: Callback) {
+func getDynamicChainInfo(callback: CallbackDynamic) {
     guard let api = URLComponents(string: chainAPI) else {
-        return callback("fail to get api")
+        return callback(nil, "fail to get api")
     }
     
     Alamofire.request(api, method: .get).responseJSON { response in
         if let error = response.error {
-            return callback("fail to get api.")
+            return callback(nil, error.localizedDescription)
         }
         
         guard let data = response.data else {
-            return callback("fail to get data")
+            return callback(nil, "fail to get response data.")
         }
         
         let json = JSON(data)
-        print(json)
-        
         let chainInfo = DynamicChainData(json: json)
-        return callback(chainInfo)
+        return callback(chainInfo, nil)
     }
 }
